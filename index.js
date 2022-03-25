d04335d7e2ec77f11dd360707fe37ae9c4fcabdf
Vitalii <vetal.shtager@gmail.com>
2022-03-25 13:54:30 -0400

    commit tree 3cc5a480b055bb9066f9a4b1599ed90b28da0601

---
+++ index.js
@@ -0,0 +1,12 @@
+function introduction(name) {
+    return `Hi, my name is ${name}.`
+}
+introduction("Aki")
+
+function introductionWithLanguage(name = "Aki", language = "Ember.js") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+
+function introductionWithLanguageOptional(name = "Gracie", language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
